160c312a882c1f03ac8c1242431cc777d1d4efc9
steven <steven@123.com>
2022-11-27 20:18:45 +0800

    timeout commit

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -4,5 +4,8 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("timeout commit");
     }
 }
