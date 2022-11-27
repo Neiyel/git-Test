d60f719dd64b7b32761e797122da518667e3bad1
steven <steven@123.com>
2022-11-27 20:23:00 +0800

    master first commit

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("master hello");
     }
 }
