bf8bb3cac417236d9da6c4d32944af390081f3ef
steven <steven@123.com>
2022-11-27 20:07:04 +0800

    second git commit by idea

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -2,7 +2,7 @@ package com.atguigu;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello git");
-
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
